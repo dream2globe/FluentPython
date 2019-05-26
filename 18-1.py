90f45b77e5454ff7902cd5675ba06e5915e56157
Seonghyeon Kang <shyeon.kang@gmail.com>
2019-05-26 14:39:53 +0900

    Added chapter 18

--- 18-1.py
+++ 18-1.py
@@ -1,3 +1,4 @@
+""" [예제 18-1] spinner_thread.py : 스레드로 텍스트 스피너 애니메이트하기 """
 import threading
 import itertools
 import time
@@ -12,15 +13,15 @@ def spin(msg, signal):
         status = char + ' ' + msg
         write(status)
         flush()
-        write('\x08' * len(status))
+        write('\x08' * len(status)) # 문자열 길이만큼 백스페이스를 반복해서 커서를 앞으로 이동시킴
         time.sleep(.1)
         if not signal.go:
             break
-    write(' ' * len(status) + '\x08' * len(status))
+    write(' ' * len(status) + '\x08' * len(status)) # 스페이스로 기존 문자열을 지우고 백스페이스로 원래 위치로 돌아감
     
 def slow_function():
-    # 입출력을 위해 기다리는 것 처럼 보임
-    time.sleep(3)
+    # 입출력을 위해 장시간 기다리는 것 처럼 보임
+    time.sleep(15) # 주 스레드에서 sleep( ) 함수를 호출할 때 GIL이 해제되므로 두 번째 스레드가 진행됨
     return 42
 
 def supervisor():
@@ -28,10 +29,10 @@ def supervisor():
     spinner = threading.Thread(target=spin,
                                args=('thinking!', signal))
     print('spinner object:', spinner)
-    spinner.start()
+    spinner.start() # spinner 스레드 시작
     result = slow_function()
     signal.go = False
-    spinner.join()
+    spinner.join()  # spinner 스레드가 끝날 때 까지 기다림
     return result
 
 def main():
